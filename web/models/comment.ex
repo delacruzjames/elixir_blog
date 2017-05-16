97c493e3f345a09c2257a85cd97b323a64e21cf5
James Dela Cruz <jcruz@assemblypayments.com>
2017-05-16 13:16:44 +0800

    elixir/phoenix blog tutorial

---
+++ web/models/comment.ex
@@ -0,0 +1,24 @@
+defmodule BlogPhoenix.Comment do
+  use BlogPhoenix.Web, :model
+
+  schema "comments" do
+    field :name, :string
+    field :content, :string
+    belongs_to :post, BlogPhoenix.Post, foreign_key: :post_id
+
+    timestamps()
+  end
+
+  @required_fields ~w(name content post_id)
+  @optional_fields ~w()
+
+  @doc """
+  Create a changeset based on the 'model' and 'params'.
+  If 'params' are nil, an invalid changeset is returned
+  with no validation perform
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
---
+++ web/models/post.ex
@@ -0,0 +1,29 @@
+defmodule BlogPhoenix.Post do
+  use BlogPhoenix.Web, :model
+  import Ecto.Query
+
+  schema "posts" do
+    field :title, :string
+    field :body, :string
+
+    has_many :comments, BlogPhoenix.Comment
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+
+  def count_comments(query) do
+   from p in query,
+     group_by: p.id,
+     left_join: c in assoc(p, :comments),
+     select: {p, count(c.id)}
+ end
+end
